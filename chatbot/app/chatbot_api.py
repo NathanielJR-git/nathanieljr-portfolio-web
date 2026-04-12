f5cbd1f2bcffd711b5444786b77ee03c458844af
Nathaniel Jonathan Rusli <omgitsnathaniels@gmail.com>
2026-04-12 20:35:45 +0700

    feat: add message field validator and slow api limiter

--- chatbot/app/chatbot_api.py
+++ chatbot/app/chatbot_api.py
@@ -1,7 +1,15 @@
 from app.query_rag_prompt import query_rag_prompt
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+
+
+# Instantiate Rate Limiter (limits based on user IP address)
+limiter = Limiter(key_func=get_remote_address)
+
 
 # Instantaite Fast API object
 app = FastAPI(
@@ -9,6 +17,12 @@ app = FastAPI(
     description="Backend API for Nathaniel's Virtual Chatbot",
     version="1.0.0"
 )
+
+
+# Setup rate limiter inside app
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
 
 # Setup middleware
 app.add_middleware(
@@ -19,30 +33,43 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 # Data model for chat request
 class ChatRequest(BaseModel):
-    message: str
+    message: str = Field(min_length=1, max_length=300)
     
+    @field_validator("message")
+    @classmethod
+    def validate_message(cls, v: str) -> str:
+        # Strip string spaces and ensure it's not totally empty afterward
+        v = v.strip()
+        if not v:
+            raise ValueError("Message cannot be empty or just whitespace.")
+        return v
+
+
 # Chatbot main endpoint for query
 @app.post("/chatbot")
-async def chatbot_endpoint(request: ChatRequest):
+@limiter.limit("10/minute") # Strict Rate Limit: 10 queries per minute per IP
+async def chatbot_endpoint(request: Request, payload: ChatRequest):
     """
     Main chatbot endpoint to send chat request via POST method,
     and receive chatbot's response based off the RAG pipeline
 
     Args:
-        request (ChatRequest): _description_
+        request (Request): Access API request details for slowAPI metadata injection
+        payload (ChatRequest): Passed chat_request via HTTP POST
 
     Raises:
         HTTPException: _description_
 
     Returns:
-        str: Nathaniel's virtual chatbot response to chat request's message
+        dict: Nathaniel's virtual chatbot response to chat request's message
     """
 
     try:
-        # Process rag prompt query
-        response = query_rag_prompt(request.message)
+        # Process rag prompt query using the stripped payload string
+        response = query_rag_prompt(payload.message)
         
         # Return response
         return {
