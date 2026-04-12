ee9e29ff2458830765260e7e556862a7a9eeac06
Nathaniel Jonathan Rusli <omgitsnathaniels@gmail.com>
2026-04-12 17:33:11 +0700

    feat: add llm model name and similarity threhsold, change embedding model name

--- chatbot/app/configuration.py
+++ chatbot/app/configuration.py
@@ -2,12 +2,14 @@
 
 class Configuration:
     # Model
-    EMBEDDING_MODEL_NAME: str = "all-MiniLM-L6-v2"
+    EMBEDDING_MODEL_NAME: str = "paraphrase-multilingual-MiniLM-L12-v2"
+    LLM_MODEL_NAME: str       = "llama-3.1-8b-instant"
     
     # RAG Parameters
     CHUNK_SIZE: int         = 600
     CHUNK_OVERLAP: int      = 100
     VECTOR_SEARCH_K: int    = 3
+    SIMILARITY_THRESHOLD    = 0.5
     
     # Path
     DATA_PATH: str       = "data"
