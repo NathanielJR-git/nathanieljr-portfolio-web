50d6f920f6e067cd4a8bf2f3e7beedf2c0b14f2c
Nathaniel Jonathan Rusli <omgitsnathaniels@gmail.com>
2026-04-16 21:49:40 +0700

    feat: add markdown splitter

--- chatbot/app/create_vector_db.py
+++ chatbot/app/create_vector_db.py
@@ -2,14 +2,14 @@ from app.configuration import config
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import MarkdownHeaderTextSplitter, RecursiveCharacterTextSplitter
 
 
 def create_vector_db():
     """
     Full vector database (Chroma DB) creation:
         1. Load raw .md documents
-        2. Split documents' content into chunks
+        2. Split documents' content into chunks by header and chunk size
         3. Embed chunks using Hugging Face Embedding model
         4. Create and load into Chroma DB
     """
@@ -23,19 +23,42 @@ def create_vector_db():
     )
     
     try:
-        documents = loader.load()
-        print(f"Loaded {len(documents)} documents from {config.DATA_PATH}")
+        markdown_documents = loader.load()
+        print(f"Loaded {len(markdown_documents)} documents from {config.DATA_PATH}")
     except Exception as e:
         print(f"Error loading documents: {e}")
         raise
     
-    # Breakdown documents into chunks
+    # Prepare markdown splitter
+    headers_to_split_on = [
+        ("#", "Header 1"),
+        ("##", "Header 2"),
+        ("###", "Header 3"),
+    ]
+    
+    markdown_splitter = MarkdownHeaderTextSplitter(
+        headers_to_split_on=headers_to_split_on,
+        strip_headers=True
+    )
+    
+    # Prepare text (chunk) splitter
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=config.CHUNK_SIZE,
         chunk_overlap=config.CHUNK_OVERLAP,
         add_start_index=True
     )
-    chunks = text_splitter.split_documents(documents)
+    
+    # Split by markdown headers
+    md_header_splits = []
+    for doc in markdown_documents:
+        splits = markdown_splitter.split_text(doc.page_content)
+        # Merge source metadata into header metadata
+        for split in splits:
+            split.metadata.update(doc.metadata)
+        md_header_splits.extend(splits)
+        
+    # Split into chunks
+    chunks = text_splitter.split_documents(md_header_splits)
     
     # Embed chunks using Hugging Face Embedding model
     embedding_model = HuggingFaceEmbeddings(
