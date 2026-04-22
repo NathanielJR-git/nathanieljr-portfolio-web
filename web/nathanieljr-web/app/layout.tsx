09d865f5b3e82248543833373883f85363892950
Nathaniel Jonathan Rusli <omgitsnathaniels@gmail.com>
2026-04-22 18:02:32 +0700

    feat: add full seo metadata

--- web/nathanieljr-web/app/layout.tsx
+++ web/nathanieljr-web/app/layout.tsx
@@ -2,9 +2,70 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "NathanielJR Portfolio Web",
-  description: "Nahtaniel Jonathan Rusli's personal portfolio website.",
-};
+  // SEO Dasar
+  title: {
+    default: 'Nathaniel Jonathan Rusli | AI and Data Enthusiast',
+    template: '%s | Nathaniel Jonathan  Rusli'
+  },
+  description: 'Nathaniel Jonathan Rusli\'s Portfolio Website.',
+  keywords: [
+    'Nathaniel Jonathan Rusli',
+    'Nathaniel JR', 
+    'Software Engineer', 
+    'AI Engineering', 
+    'ITB', 
+    'Quantitative Finance', 
+    'Machine Learning',
+    'Data Science' 
+  ],
+  authors: [{ name: 'Nathaniel Jonathan Rusli' }],
+  creator: 'Nathaniel Jonathan Rusli',
+
+  // OpenGraph (Preview for LinkedIn and WhatsApp)
+  openGraph: {
+    title: 'Nathaniel Jonathan Rusli | AI and Data Enthusiast',
+    description: 'Nathaniel Jonathan Rusli\'s Portfolio Website',
+    url: 'https://nathanieljr.dev',
+    siteName: 'Nathaniel Portfolio',
+    images: [
+      {
+        url: '/memojis/nate-memoji-happy.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Nathaniel Jonathan Portfolio Preview',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+
+  // Twitter Card
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Nathaniel Jonathan Rusli | AI and Data Enthusiast',
+    description: 'Nathaniel Jonathan Rusli\'s Portfolio Website',
+    images: ['/memojis/nate-memoji-happy.jpg'],
+  },
+
+  // SEO Robot
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+
+  // Icons
+  icons: {
+    icon: '/icon.png',
+    apple: '/apple-touch-icon.png',
+  },
+}
 
 export default function RootLayout({
   children,
